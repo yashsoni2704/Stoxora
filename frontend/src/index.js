36fc915ab321a8d4b893834fd9a5164854300347
Yash Soni <yashrakeshsoni@gmail.com>
2025-09-09 07:37:18 +0530

    Component-Structure Setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,5 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <h1>Hello</h1>
-    </div>
   </React.StrictMode>
 );
